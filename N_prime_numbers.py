d49efe13656e72b6ad647d82e205fd9fadf6770d
SHANKUCODES <smkdpld@gmail.com>
2026-07-08 01:21:08 +0530

    Updated N prime numbers program

--- N_prime_numbers.py
+++ N_prime_numbers.py
@@ -29,13 +29,3 @@ while count < n:
 
 
             
-# num=int(input("Enter a number="))
-
-# if num<=1:
-#     print(f"number {num} is not a prime number")
-
-#     for i in range(2,num):
-#         if num%i==0:
-#             print(num,"is a prime number ")
-#         else:
-#             print(num,"is not a prime number")
